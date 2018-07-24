4239ecc359eb06eae265475178cb01f811cdaa07
Mats <mats.becker@gmail.com>
2018-07-24 20:25:45 +0200

    Added while loop

--- app.js
+++ app.js
@@ -1,64 +1,82 @@
 // before running, either globally install kafka-node  (npm install kafka-node)
 // or add kafka-node to the dependencies of the local application
- 
-var kafka = require('kafka-node')
-var Producer = kafka.Producer
+
+var kafka = require("kafka-node");
+var Producer = kafka.Producer;
 KeyedMessage = kafka.KeyedMessage;
- 
+
 var client;
 KeyedMessage = kafka.KeyedMessage;
- 
-var APP_VERSION = "0.8.5"
-var APP_NAME = "KafkaProducer"
- 
+
+var APP_VERSION = "0.8.5";
+var APP_NAME = "KafkaProducer";
+
 var topicName = "a516817-kentekens";
-var KAFKA_BROKER_IP = '212.47.241.204:32400';
- 
+var KAFKA_BROKER_IP = "212.47.241.204:32400";
+
 // from the Oracle Event Hub - Platform Cluster Connect Descriptor
 var kafkaConnectDescriptor = KAFKA_BROKER_IP;
- 
+
 console.log("Running Module " + APP_NAME + " version " + APP_VERSION);
- 
+
 function initializeKafkaProducer(attempt) {
   try {
-    console.log(`Try to initialize Kafka Client at ${kafkaConnectDescriptor} and Producer, attempt ${attempt}`);
+    console.log(
+      `Try to initialize Kafka Client at ${kafkaConnectDescriptor} and Producer, attempt ${attempt}`
+    );
     const client = new kafka.KafkaClient({ kafkaHost: kafkaConnectDescriptor });
     console.log("created client");
     producer = new Producer(client);
     console.log("submitted async producer creation request");
-    producer.on('ready', function () {
+    producer.on("ready", function() {
       console.log("Producer is ready in " + APP_NAME);
     });
-    producer.on('error', function (err) {
-      console.log("failed to create the client or the producer " + JSON.stringify(err));
-    })
-  }
-  catch (e) {
+    producer.on("error", function(err) {
+      console.log(
+        "failed to create the client or the producer " + JSON.stringify(err)
+      );
+    });
+  } catch (e) {
     console.log("Exception in initializeKafkaProducer" + JSON.stringify(e));
     console.log("Try again in 5 seconds");
     setTimeout(initializeKafkaProducer, 5000, ++attempt);
   }
-}//initializeKafkaProducer
+} //initializeKafkaProducer
 initializeKafkaProducer(1);
- 
+
 var eventPublisher = module.exports;
- 
-eventPublisher.publishEvent = function (eventKey, event) {
+
+eventPublisher.publishEvent = function(eventKey, event) {
   km = new KeyedMessage(eventKey, JSON.stringify(event));
-  payloads = [
-    { topic: topicName, messages: [km], partition: 0 }
-  ];
-  producer.send(payloads, function (err, data) {
+  payloads = [{ topic: topicName, messages: [km], partition: 0 }];
+  producer.send(payloads, function(err, data) {
     if (err) {
-      console.error("Failed to publish event with key " + eventKey + " to topic " + topicName + " :" + JSON.stringify(err));
+      console.error(
+        "Failed to publish event with key " +
+          eventKey +
+          " to topic " +
+          topicName +
+          " :" +
+          JSON.stringify(err)
+      );
     }
-    console.log("Published event with key " + eventKey + " to topic " + topicName + " :" + JSON.stringify(data));
+    console.log(
+      "Published event with key " +
+        eventKey +
+        " to topic " +
+        topicName +
+        " :" +
+        JSON.stringify(data)
+    );
   });
- 
-}
- 
+};
+
 //example calls: (after waiting for three seconds to give the producer time to initialize)
-setTimeout(function () {
-  eventPublisher.publishEvent("mykey", { "kenteken": "56-TAG-2", "country": "nl" })
-}
-  , 3000)
+setTimeout(function() {
+  let i,
+    x = 0;
+  while (i < 10) {
+    eventPublisher.publishEvent("mykey", { msg: x, kafka: "aareon" });
+    x = x + 1;
+  }
+}, 3000);
