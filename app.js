29b85438409759c6d4a3174aec3e07d940d23058
Mats <mats.becker@gmail.com>
2018-07-24 22:43:03 +0200

    Added kafka performance test

--- app.js
+++ app.js
@@ -90,5 +90,32 @@ setTimeout(function() {
 
 /* Kafka Performance Test
 https://community.hortonworks.com/content/supportkb/151880/errorexception-thrown-by-the-agent-javarmiserverex.html
-./bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance --throughput=1000 --topic=MY_TOPIC --num-records=50000000 --record-size=200 --producer-props bootstrap.servers=212.47.241.204:32400 buffer.memory=67108864 batch.size=64000
+./bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance --throughput=2000000 --topic=MY_TOPIC --num-records=50000000 --record-size=2000 --producer-props bootstrap.servers=212.47.241.204:32400 buffer.memory=67108864 batch.size=64000
 */
+
+https://prasanthkothuri.wordpress.com/2016/10/04/benchmarking-apache-kafka-on-openstack-vms/
+
+Single producer publishing 1000 byte messages with no replication
+# create kafka topic
+./bin/kafka-topics.sh --zookeeper zookeeper.kafka:2181 --create --topic test --partitions 48 --replication-factor 1
+# run the producer to publish events to Kafka topic
+bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance --topic test --num-records 50000000 --record-size 1000 --throughput -1 --producer-props acks=1 bootstrap.servers=212.47.241.204:32400 buffer.memory=104857600 batch.size=9000
+
+Single producer publishing 500 byte messages with (3x) and with out replication
+The objective of this test is to understand the cost of the replication
+
+# create kafka topic (with replication)
+bin/kafka-topics.sh --zookeeper habench001:2181 --create --topic testr3 --partitions 48 --replication-factor 3
+# publish messages to kafka topic with required settings
+bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance --topic testr3 --num-records 30000000 --record-size 500 --throughput -1 --producer-props acks=1 bootstrap.servers=habench101:9092 buffer.memory=104857600 batch.size=6000
+
+Three producers, 3x async replication with different message sizes
+The object of the test is to understand the effect of the message size on the producer throughput
+
+# publish 200 byte messages to kafka topic
+bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance --topic testr3 --num-records 30000000 --record-size 200 --throughput -1 --producer-props acks=1 bootstrap.servers=habench101:9092 buffer.memory=104857600 batch.size=6000
+# publish 500 byte messages to kafka topic
+bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance --topic testr3 --num-records 15000000 --record-size 500 --throughput -1 --producer-props acks=1 bootstrap.servers=habench101:9092 buffer.memory=104857600 batch.size=6000
+
+# publish 1000 byte messages to kafka topic
+bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance --topic testr3 --num-records 10000000 --record-size 1000 --throughput -1 --producer-props acks=1 bootstrap.servers=habench101:9092 buffer.memory=104857600 batch.size=6000
